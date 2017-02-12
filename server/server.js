e142d8bfdd3e4773a5ccb686cd29337d7de0e8b1
Ian Moore <ianmooregis@gmail.com>
2017-02-11 22:05:07 -0600

    modified routes, added regex for URL validation

--- server/server.js
+++ server/server.js
@@ -12,13 +12,19 @@ app.listen(port, () => {
     console.log(`Started up at port ${port}`);
 });
 
-app.get('/new/:id', (req, res) => {
+app.get('/new/:url*', (req, res) => {
+    if (!validateURL(req.url.slice(5))) {
+        return res.status(400).send({error: "Invalid URL"});
+    }
     var newurl = new Urls({
-        url: req.params.id,
+        url: req.url.slice(5),
         shortlink: Math.random().toString(36).substr(2, 6)
     });
     newurl.save().then((doc) => {
-        res.send(doc);
+        res.send({
+            url: doc.url,
+            shortlink: doc.shortlink
+        });
     }, (e) => {
         res.status(400).send(e);
     });
@@ -28,32 +34,19 @@ app.get('/:id', (req, res) => {
     Urls.find({
         shortlink: req.params.id
     }).then((url) => {
-        //res.send({url});
-        res.redirect(url[0].url);
+        let redirecturl = url[0].url;
+        //console.log(redirecturl);
+        res.redirect(redirecturl);
     }, (e) => {
-        res.status(400).send({error: 'shortened URL not found'});
+        res.status(404).send({error: 'shortened URL ID not found'});
     });
 
 });
 
-app.post('/add', (req, res) => {
-    var newurl = new Urls({
-        url: req.body.url,
-        shortlink: Math.random().toString(36).substr(2, 6)
-    });
-    newurl.save().then((doc) => {
-        res.send(doc);
-    }, (e) => {
-        res.status(400).send(e);
-    });
-});
-
-//app.get('/urls', (req,res) => {
-//    Urls.find().then((urls) => {
-//        res.send({urls});
-//    }, (e) => {
-//        res.status(400).send(e);
-//    });
-//});
+const validateURL =(url)=> {
+    // regex source: https://gist.github.com/dperini/729294
+    var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+    return regex.test(url);
+};
 
 module.exports = {app};
