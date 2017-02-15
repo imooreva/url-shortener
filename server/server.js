33d44290ca2b45d759120c3ec155698ed67665ab
Ian Moore <ianmooregis@gmail.com>
2017-02-14 18:43:41 -0600

    New file for functions

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const express = require('express');
-const {ObjectID} = require('mongodb');
 const port = process.env.PORT || 3000; //port used for Heroku, otherwise 3000
 
-var {mongoose} = require('./db/mongoose.js');
-var {Urls} = require('./models/urls.js');
+const {mongoose} = require('./db/mongoose.js');
+const {Urls} = require('./models/urls.js');
+const {validateURL, createNewURL} = require('./functions.js');
 
 var app = express();
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 app.use(express.static('./public'));
 app.listen(port, () => console.log(`Started up on port ${port}`));
 
@@ -30,25 +30,12 @@ app.get('/:id', (req, res) => {
         shortlink: req.params.id
     }).then((url) => {
         let redirecturl = url[0].url;
-        //console.log(redirecturl);
+        //console.log('Redirect to:', redirecturl);
         res.redirect(redirecturl);
     }).catch((e) => {
         res.status(404).send({error: 'shortened URL ID not found'});
     });
 
 });
-
-const validateURL = (url) => {
-    // regex source: https://gist.github.com/dperini/729294
-    var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-    return regex.test(url);
-};
-
-const createNewURL = (i) => {
-    return new Urls({
-        url: i,
-        shortlink: Math.random().toString(36).substr(2, 6)
-    });
-};
 
 module.exports = {app};
