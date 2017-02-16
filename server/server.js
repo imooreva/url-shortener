3318f8b69b493e3d371f7aaf9cc1bb155c99b06d
Ian Moore <ianmooregis@gmail.com>
2017-02-15 22:15:12 -0600

    minor edits

--- server/server.js
+++ server/server.js
@@ -1,21 +1,20 @@
-//const bodyParser = require('body-parser');
 const express = require('express');
 const port = process.env.PORT || 3000; //port used for Heroku, otherwise 3000
 
 const {mongoose} = require('./db/mongoose.js');
 const {Urls} = require('./models/urls.js');
-const {validateURL, createNewURL} = require('./functions.js');
+const {validateURL, createNewURL, checkForDuplicates} = require('./functions.js');
 
 var app = express();
-//app.use(bodyParser.json());
 app.use(express.static('./public'));
 app.listen(port, () => console.log(`Started up on port ${port}`));
 
 app.get('/new/:url*', (req, res) => {
-    if (!validateURL(req.url.slice(5))) {
+    let slicedURL = req.url.slice(5);
+    if (!validateURL(slicedURL)) {
         return res.status(400).send({error: "Invalid URL"});
     }
-    createNewURL(req.url.slice(5)).save().then((doc) => {
+    createNewURL(slicedURL).save().then((doc) => {
         res.send({
             url: doc.url,
             shortlink: doc.shortlink
