f60a63ce3adf681ad29179852c2f686194196d64
Ian Moore <ianmooregis@gmail.com>
2017-04-05 01:05:16 -0400

    Fixed two tests

--- server/models/urls.test.js
+++ server/models/urls.test.js
@@ -4,7 +4,7 @@ const {validateURL} = require('.././functions.js');
 const expect = require('expect');
 const request = require('supertest');
 
-describe('GET /new', () => {
+describe('URL tests', () => {
     
     it('should generate a new shortened URL as a string with six characters', (done) => {
         request(app.listen())
@@ -15,18 +15,15 @@ describe('GET /new', () => {
                 expect(res.body.shortlink).toBeA('string')
                 expect(res.body.shortlink.length).toBe(6)
             })
-            done();
+            .end(done);
     });
     
     it('should forward user when existing shortened URL passed in app URL', (done) => {
         request(app.listen())
             //shortlink g3i067 is stored in local database and redirects to grubhub.com
             .get('/g3i067')
-            .on('response', (res) => {
-                expect(302)
-                console.log(res)
-            })
-            done();
+            .expect(302)
+            .end(done);
     });
     
     it('should forward user when generated shortened URL passed in app URL', (done) => {
