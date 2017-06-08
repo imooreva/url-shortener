a63f460cf52304d7065789e3dbcece34e9a84ff7
Ian Moore <ianmooregis@gmail.com>
2017-06-08 00:00:06 -0400

    Modified code to prevent duplicate entries

--- server/functions.js
+++ server/functions.js
@@ -1,6 +1,6 @@
 var {Urls} = require('./models/urls.js');
 
-var validateURL = (url) => {
+var validateURL = url => {
     // regex source: https://gist.github.com/dperini/729294
     let regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
     return regex.test(url);
@@ -12,17 +12,25 @@ var createNewURL = (req, res) => {
     if (!validateURL(slicedURL)) {
         return res.status(400).send({error: 'Invalid URL'});
     }
-    //create and save new URL shortened URL to mongoDB
-    new Urls({
-        url: slicedURL,
-        shortlink: Math.random().toString(36).substr(2, 6)
-    }).save().then((doc) => {
-        res.send({
-            url: doc.url,
-            shortlink: doc.shortlink
-        });
-    }).catch((e) => {
-        res.status(500).send(e);
+    //Search for existing URL in database and send if query returns result
+    //If nothing returned, create and save new shortened URL document
+    Urls.findOne({
+        url: slicedURL
+    }, (err, stored) => {
+        if (err) { return }
+        if (stored) {
+            console.log('it does exist');
+            return res.status(200).send(stored);
+        } else {
+            let newURL = new Urls({
+                url: slicedURL,
+                shortlink: Math.random().toString(36).substr(2, 6)
+            });
+            newURL.save((err) => {
+                if (err) { return }
+                res.status(200).send(newURL);
+            });
+        }
     });
 };
 
